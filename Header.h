9c30c863d279de798f49195144a4213fdb7e094f
theRaiven <esamap2@mail.ru>
2024-04-14 11:29:23 +0700

    PascelCase

--- Header.h
+++ Header.h
@@ -1,8 +1,6 @@
 ﻿#pragma once
 
 #include <iostream>
-#include <Windows.h>
-using namespace std;
 
 struct Tree;
 
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 //
 
 #include "Header.h"
-
+using namespace std;
 
 int main()
 {
